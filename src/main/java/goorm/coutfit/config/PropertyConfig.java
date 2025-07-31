4c9285fb8c78f7c85ba46039087f0bbb894fa34c
jiyeonyh <wldus8827@gmail.com>
2025-07-31 17:02:28 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/goorm/coutfit/config/PropertyConfig.java
@@ -0,0 +1,12 @@
+package goorm.coutfit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:properties/env.local.properties")
+})
+public class PropertyConfig {
+}
---
+++ src/test/java/goorm/coutfit/CoutfitApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.coutfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoutfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
