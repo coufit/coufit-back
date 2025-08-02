ec3854d747ba7f42d8675fb132dd99db35c0d9a1
jiyeonyh <wldus8827@gmail.com>
2025-08-02 23:40:03 +0900

    feat: 이번달 절약 정보 제거 및 총 사용 금액 추가

--- src/main/java/goorm/coutfit/point/controller/response/PointSummaryResponse.java
+++ src/main/java/goorm/coutfit/point/controller/response/PointSummaryResponse.java
@@ -7,15 +7,12 @@ public class PointSummaryResponse {
     private Long monthlyUsageAmount;
     private Long monthlyUsageCount;
     private Long usedStoresCount;
-    private Integer monthlySavedAmount;
+    private Long totalUsageAmount;
 
-    public PointSummaryResponse(Long monthlyUsageAmount, Long monthlyUsageCount, Long usedStoresCount, Integer monthlySavedAmount) {
-        // TODO: 이번달 사용 내역의 10% 절약 했다고 가정
-        int savedAmount = (int) (monthlyUsageAmount * 0.1);
-        
+    public PointSummaryResponse(Long monthlyUsageAmount, Long monthlyUsageCount, Long usedStoresCount, Long totalUsageAmount) {
         this.monthlyUsageAmount = monthlyUsageAmount;
         this.monthlyUsageCount = monthlyUsageCount;
         this.usedStoresCount = usedStoresCount;
-        this.monthlySavedAmount = savedAmount;
+        this.totalUsageAmount = totalUsageAmount;
     }
 }
